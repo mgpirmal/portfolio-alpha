a21fb89c2ee11093f2b861f37429bb20653cf4d4
Mattchu <MGPIRMAL678@GMAIL.COM>
2021-05-12 17:19:11 -0400

    images

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,6 @@
 import React, {useEffect,useState} from "react";
 import sanityClient from "../client.js";
-import image from "../whitewood.jpg"
+
 
 export default function Project(){
     
@@ -30,18 +30,13 @@ export default function Project(){
     
     return(
         <main>
-             <img 
-                 src={image} 
-                 alt= "beach Background" 
-                 className="absolute object-cover w-full h-full"/>
-        <div className="bg-green-100 min-h-screen p-12">
+        <div className="bg-gray-700 min-h-screen p-12">
             
             <section className="container mx-auto">
-                <h1 className="text-5xl flex justify-center cursive">My Projects</h1>
-                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Treasure is Buried Here!</h2>
+                <h1 className="text-5xl flex justify-center cursive text-white mb-12">My Projects</h1>
                 <section className="grid grid-cols-2 gap-8">
                    {projectData && projectData.map((project, index) => (
-                    <article className="relative rounded-lg shadow-xl bg-white  p-16" >
+                    <article className="relative rounded-lg shadow-xl bg-green-700 p-16" >
                       
                         <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
                             <a
@@ -51,8 +46,12 @@ export default function Project(){
                             rel="noopener noreferrer">{project.title}</a>
                         </h3>
                             <div className="text-gray-500 text-xs space-x-4">
+                            <img 
+                             src={project.mainImage.asset.url}
+                             alt={project.mainImage.alt}
+                             className="w-full h-full rounded-r border-8 border-gray-700"/>
                                 <span ><strong className="font-bold">Completed on:</strong>{" "}{new Date(project.date).toLocaleDateString()}</span>
-                                <p className="my-6 text-lg text-700 leading-relaxed">{project.description}</p>
+                                <p className="my-6 text-lg text-green-200 leading-relaxed">{project.description}</p>
                                 <a href={project.link} rel="noopener noreferrer" target="_blank" className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl">
                                 View The Project{" "}
                                 <span role="img" aria-label="👀"></span>
